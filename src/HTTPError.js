72c086de75e49037823691b7086c404efae20a31
Elie Rotenberg <elie@rotenberg.io>
2014-11-14 15:06:50 +0100

    Renamed httpCode -> statusCode, httpDesc -> statusText Provided getters HTTPError#getStatusCode, HTTPError#getStatusText, HTTPError#getReason.

--- src/HTTPError.js
+++ src/HTTPError.js
@@ -2,24 +2,36 @@ const _ = require('lodash-next');
 
 class HTTPError extends Error {
   constructor(message, opts = {}) {
-    let { code, desc } = opts;
-    this.httpCode = code || this.httpCode;
-    this.httpDesc = desc || this.httpDesc;
+    let { statusCode, statusText } = opts;
+    this.statusCode = code || this.statusCode;
+    this.statusText = desc || this.statusText;
     this.message = message;
+  }
+
+  getStatusCode() {
+    return this.statusCode;
+  }
+
+  getStatusText() {
+    return this.statusText;
+  }
+
+  getReason() {
+    return this.message;
   }
 }
 
 _.extend(HTTPError.prototype, {
-  httpCode: null,
-  httpDesc: null,
+  statusCode: null,
+  statusText: null,
   name: 'HTTPError',
 });
 
 _.extend(HTTPError, {
-  extend(httpCode, httpDesc, name) {
+  extend(statusCode, statusText, name) {
     _.dev(() => code.should.be.a.Number && desc.should.be.a.String);
     class SpecificHTTPError extends HTTPError {}
-    _.extend(SpecificHTTPError.prototype, { httpCode, httpDesc, name });
+    _.extend(SpecificHTTPError.prototype, { statusCode, statusText, name });
   },
 });
 
